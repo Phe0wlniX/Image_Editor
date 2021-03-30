9e9a6de1bf2e5969390ec099ed57f9cc6029efe7
Phe0wlnix <jarah@gmail.com>
2021-03-30 18:23:54 +0700

    Image_Editor_0.2

--- Image_Editor/Form1.Designer.cs
+++ Image_Editor/Form1.Designer.cs
@@ -28,13 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.File = new System.Windows.Forms.ToolStripMenuItem();
+            this.bt_download = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save = new System.Windows.Forms.ToolStripMenuItem();
+            this.Create_new = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_View = new System.Windows.Forms.ToolStripMenuItem();
+            this.bt_open_window = new System.Windows.Forms.ToolStripMenuItem();
+            this.bt_close_window = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.bt_instument = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.p_color = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.File,
+            this.menu_View});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // File
+            // 
+            this.File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bt_download,
+            this.Save,
+            this.Create_new});
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(48, 20);
+            this.File.Text = "Файл";
+            // 
+            // bt_download
+            // 
+            this.bt_download.Name = "bt_download";
+            this.bt_download.Size = new System.Drawing.Size(180, 22);
+            this.bt_download.Text = "Загрузить";
+            // 
+            // Save
+            // 
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(180, 22);
+            this.Save.Text = "Сохранить как";
+            // 
+            // Create_new
+            // 
+            this.Create_new.Name = "Create_new";
+            this.Create_new.Size = new System.Drawing.Size(180, 22);
+            this.Create_new.Text = "Создать новый";
+            // 
+            // menu_View
+            // 
+            this.menu_View.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bt_open_window,
+            this.bt_close_window});
+            this.menu_View.Name = "menu_View";
+            this.menu_View.Size = new System.Drawing.Size(39, 20);
+            this.menu_View.Text = "Вид";
+            // 
+            // bt_open_window
+            // 
+            this.bt_open_window.Name = "bt_open_window";
+            this.bt_open_window.Size = new System.Drawing.Size(213, 22);
+            this.bt_open_window.Text = "Открыть окно рисования";
+            // 
+            // bt_close_window
+            // 
+            this.bt_close_window.Name = "bt_close_window";
+            this.bt_close_window.Size = new System.Drawing.Size(213, 22);
+            this.bt_close_window.Text = "Закрыть окно рисования";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.bt_instument});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // bt_instument
+            // 
+            this.bt_instument.Name = "bt_instument";
+            this.bt_instument.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.bt_instument.Size = new System.Drawing.Size(74, 17);
+            this.bt_instument.Text = "Инструмент";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 404);
+            this.panel2.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel3);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Кисть";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.p_color);
+            this.panel3.Location = new System.Drawing.Point(3, 16);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 157);
+            this.panel3.TabIndex = 0;
+            // 
+            // p_color
+            // 
+            this.p_color.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.p_color.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p_color.Location = new System.Drawing.Point(3, 23);
+            this.p_color.Name = "p_color";
+            this.p_color.Size = new System.Drawing.Size(56, 24);
+            this.p_color.TabIndex = 5;
+            this.p_color.Tag = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 106);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem File;
+        private System.Windows.Forms.ToolStripMenuItem bt_download;
+        private System.Windows.Forms.ToolStripMenuItem Save;
+        private System.Windows.Forms.ToolStripMenuItem Create_new;
+        private System.Windows.Forms.ToolStripMenuItem menu_View;
+        private System.Windows.Forms.ToolStripMenuItem bt_open_window;
+        private System.Windows.Forms.ToolStripMenuItem bt_close_window;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel bt_instument;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel p_color;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
